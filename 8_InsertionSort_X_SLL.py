4b9eb410a64aec1e65552f558075ebf54aa0d69c
GedePakusadewa <79703224+gedePakusadewa@users.noreply.github.com>
2024-02-23 15:28:30 +0800

    Update 8_InsertionSort_X_SLL.py

--- 8_InsertionSort_X_SLL.py
+++ 8_InsertionSort_X_SLL.py
@@ -1,6 +1,6 @@
 class Node():
 	def __init__(self, data=None):
-		self.data = dataNode
+		self.data = data
 		self.next = None
 
 class SLL():
@@ -8,18 +8,37 @@ class SLL():
 		self.headNode = None
 
 	def print_(self):
+		temp = ""
 		dataNode = self.headNode
 		while dataNode is not None:
-			print(dataNode.data)
+			temp += " " + str(dataNode.data)
 			dataNode = dataNode.next
+		print(temp)
 
 	# insert in last node
-	def insert(self, data):
+	def insert_in_last(self, data):
+		if self.headNode is None:
+			self.headNode = Node(data)
+
+			return
+
 		dataNode = self.headNode
 		while dataNode.next is not None:
 			dataNode = dataNode.next
 
 		dataNode.next = Node(data)
+
+	def insert_in_first(self, data):
+		tempNode = self.headNode
+		self.headNode = Node(data)
+		self.headNode.next = tempNode
+
+	def insert_between(self, node, data):
+		temp = Node(data)
+
+		temp.next = node.next
+		node.next = temp
+
 
 class InsertionSort():
 	def __init__(self, data=None):
@@ -33,22 +52,48 @@ class InsertionSort():
 		dataNode = SLL()
 
 		for idx in range(len(listData)):
-			dataNode.insert(listData[idx])
+			dataNode.insert_in_last(listData[idx])
 
 		return dataNode
 
 	def asc_sort(self):
 		node = self.dataNode.headNode
-		sorted_node = None
+		sorted_node = SLL()
 
 		while node is not None:
-
-			sorted_node = tes1(sorted_node, node.data)
+			sorted_node = self.sort_the_node(sorted_node, node)
 
 			node = node.next
 
-	def tes1(self, curr_node, data):
-		head = 
+		self.dataNode = sorted_node
+
+	def sort_the_node(self, node, curr_data):
+
+		if node.headNode is None:
+			node.insert_in_last(curr_data.data)
+
+			return node
+
+		if node.headNode.data > curr_data.data:
+			node.insert_in_first(curr_data.data)
+		else:
+			curr = node.headNode
+			while curr.next is not None and curr.next.data < curr_data.data:
+				curr = curr.next
+				
+			node.insert_between(curr, curr_data.data)
+
+		return node
+
+	def print_(self):
+		self.dataNode.print_()
+
+
+t1 = InsertionSort([73, 26, 99, -33, 42, -52, 44, -33, 65, -53])
+# t1.print_()
+t1.asc_sort()
+t1.print_()
+
 
 
 
